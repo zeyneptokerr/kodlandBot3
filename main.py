5ea3d16a55363944af85eb5f9dd59665ce912e8f
zeyneptokerr <50079218+zeyneptokerr@users.noreply.github.com>
2024-01-11 21:25:40 +0300

    Create main.py
    terminale python main.py yazarak çalıştırılmalı.

---
+++ main.py
@@ -0,0 +1,25 @@
+import discord
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("token eklenecek")
